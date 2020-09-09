23a9b86b0a5b7359022174f0115fcbf4a67429df
Kostas <44273616+spkostas@users.noreply.github.com>
2020-09-09 23:55:39 +0300

    Create index.js

---
+++ index.js
@@ -0,0 +1,9 @@
+<script type="text/javascript">
+    function goToNewPage()
+    {
+        var url = document.getElementById('list').value;
+        if(url != 'none') {
+            window.location = url;
+        }
+    }
+</script>
